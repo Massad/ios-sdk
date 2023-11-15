72549cf1d2b04280b7dcc542e12e32b24b9e3bdf
Omar Masad <3076722+Massad@users.noreply.github.com>
2023-11-15 11:50:55 +0400

    Changes

---
+++ Package.swift
@@ -0,0 +1,17 @@
+// swift-tools-version: 5.6
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "TestAppIOSDK",
+    platforms: [.iOS("13.0")],
+    products: [
+        // Products define the executables and libraries a package produces, and make them visible to other packages.
+        .library(name: "TestAppIOSDK", targets: ["TestAppIOSDK"]),
+    ],
+    dependencies: [],
+    targets: [
+        .binaryTarget(name: "TestAppIOSDK", path: "TestAppIOSDK.xcframework"),
+    ]
+)
